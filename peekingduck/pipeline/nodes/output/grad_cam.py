5574ef9df2cba3b0ea812a14d9df1b6d33fb5830
Gao Hongnan <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2022-11-01 20:54:31 +0800

    add gradcam func to node

--- peekingduck/pipeline/nodes/output/grad_cam.py
+++ peekingduck/pipeline/nodes/output/grad_cam.py
@@ -19,6 +19,7 @@ Shows the outputs on your display.
 from typing import Any, Dict, Union
 
 import cv2
+import numpy as np
 
 from peekingduck.pipeline.nodes.abstract_node import AbstractNode
 
@@ -93,3 +94,65 @@ class Node(AbstractNode):
 
     # def _get_config_types(self) -> Dict[str, Any]:
     #     """Returns dictionary mapping the node's config keys to respective types."""
+
+
+def show_gradcam(image):
+    pass
+
+
+def show_resnet_gradcam(
+    self,
+    model,
+    image: np.ndarray,
+    original_image: np.ndarray,
+    plot_gradcam: bool = True,
+) -> np.ndarray:
+    """Show the gradcam of the image for ResNet variants.
+    This will not work on other types of network architectures.
+    Args:
+        image (np.ndarray): The input image.
+        original_image (np.ndarray): The original image.
+        plot_gradcam (bool): If True, will plot the gradcam. Defaults to True.
+    Returns:
+        gradcam_image (np.ndarray): The gradcam image.
+    """
+    # model = self.resnet50d
+    # only for resnet variants! for simplicity we don't enumerate other models!
+    # target_layers = [model.model.layer4[-1]]  # [model.backbone.layer4[-1]]
+    target_layers = [model.layer4[-1]]
+    reshape_transform = None
+
+    # input is np array so turn to tensor as well as resize aand as well as turn to tensor by totensorv2
+    # so no need permute since albu does it
+    image = self.transforms(image=image)["image"].to(self.device)
+
+    # if image tensor is 3 dim, unsqueeze it to 4 dim with 1 in front.
+    image = image.unsqueeze(0)
+    gradcam = GradCAM(
+        model=model,
+        target_layers=target_layers,
+        use_cuda=False,
+        reshape_transform=reshape_transform,
+    )
+
+    # # If targets is None, the highest scoring category will be used for every image in the batch.
+    gradcam_output = gradcam(
+        input_tensor=image,
+        target_category=None,
+        aug_smooth=False,
+        eigen_smooth=False,
+    )
+    original_image = original_image / 255.0
+
+    gradcam_image = show_cam_on_image(original_image, gradcam_output[0], use_rgb=False)
+
+    if plot_gradcam:
+        _fig, axes = plt.subplots(figsize=(8, 8), ncols=2)
+        axes[0].imshow(original_image)
+        # axes[0].set_title(f"y_true={y_true:.4f}")
+        axes[1].imshow(gradcam_image)
+        # axes[1].set_title(f"y_pred={y_pred}")
+        plt.show()
+        torch.cuda.empty_cache()
+
+    return gradcam_image
