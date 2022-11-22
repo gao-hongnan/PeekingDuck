30b2854ee972204055a9afaa8e56963da47bdebc
gaohn <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2022-11-22 13:04:58 +0800

    refactor: update gradcam code

--- peekingduck/pipeline/nodes/output/grad_cam.py
+++ peekingduck/pipeline/nodes/output/grad_cam.py
@@ -16,6 +16,7 @@
 Shows the outputs on your display.
 """
 
+from dataclasses import dataclass
 from typing import Any, Dict, Union
 
 import cv2
@@ -156,3 +157,90 @@ def show_resnet_gradcam(
         torch.cuda.empty_cache()
 
     return gradcam_image
+
+
+use_cuda = True if device == "cuda" else False
+# reshape_transform = None
+target_layers = [model.layer4[-1]]
+
+# Construct the CAM object once, and then re-use it on many images:
+cam = GradCAM(model=model, target_layers=target_layers, use_cuda=use_cuda)
+
+# We have to specify the target we want to generate
+# the Class Activation Maps for.
+# If targets is None, the highest scoring category
+# will be used for every image in the batch.
+# Here we use ClassifierOutputTarget, but you can define your own custom targets
+# That are, for example, combinations of categories, or specific outputs in a non standard model.
+target_category = [ClassifierOutputTarget(243)]
+
+# You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
+grayscale_cam = cam(input_tensor=image_tensor, targets=target_category)
+print(grayscale_cam.shape)
+# In this example grayscale_cam has only one image in the batch:
+grayscale_cam = grayscale_cam[0, :]
+print(grayscale_cam.shape)
+
+# image_numpy = cv2.resize(image_tensor_to_numpy, (image_size, image_size)) # resize
+image_normalized = image_numpy / 255.0
+# image_normalized  = image_tensor_to_numpy/255.
+visualization = show_cam_on_image(image_normalized, grayscale_cam, use_rgb=True)
+
+
+@dataclass(frozen=False, init=True)
+class GradCamWrapper:
+    model: nn.Module
+    target_layers: List[nn.Module]
+    device: str
+    targets: List[int]
+    image_tensor: torch.Tensor
+    image_numpy: np.ndarray
+
+    reshape_transform: Optional[Callable] = None
+
+    target_categories: List[ClassifierOutputTarget] = field(init=False)
+    use_cuda: bool = field(init=False)
+
+    def __post_init__(self):
+        self.use_cuda = self.device == "cuda"
+        self.target_categories = [
+            ClassifierOutputTarget(target) for target in self.targets
+        ]
+        self.gradcam = self._init_gradcam_object()
+
+    def _init_gradcam_object(self) -> GradCam:
+        return GradCAM(
+            model=self.model,
+            target_layers=self.target_layers,
+            use_cuda=self.use_cuda,
+            reshape_transform=self.reshape_transform,
+        )
+
+    def _generate_heatmap(self) -> np.ndarray:
+        heatmap = self.gradcam(
+            input_tensor=self.image_tensor,
+            targets=self.target_categories,
+        )
+        return heatmap
+
+    def display(self) -> None:
+        heatmap = self._generate_heatmap()
+        heatmap = heatmap[0, :]
+        visualization = show_cam_on_image(self.image_numpy, heatmap, use_rgb=True)
+        fig, axes = plt.subplots(figsize=(20, 10), ncols=3)
+
+        axes[0].imshow(self.image_numpy)
+        axes[0].axis("off")
+
+        axes[1].imshow(heatmap)
+        axes[1].axis("off")
+
+        axes[2].imshow(visualization)
+        axes[2].axis("off")
+
+        plt.show()
+
+    def __del__(self):
+        del self.model
+        del self.target_layers
+        del self.use_cuda
